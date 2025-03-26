93b57c8a5fbf0f4d763178dc7ef970c077ba4eee
tttien1812 <tttien204@gmail.com>
2025-03-26 17:49:13 +0700

    add table allcode

---
+++ src/models/allcode.js
@@ -0,0 +1,27 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // định danh các mối quan hệ
+    }
+  }
+  User.init(
+    {
+      key: DataTypes.STRING,
+      type: DataTypes.STRING,
+      valueEN: DataTypes.STRING,
+      valueVI: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Allcode",
+    }
+  );
+  return Allcode;
+};
