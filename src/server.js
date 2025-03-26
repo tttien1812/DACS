01da0c7b4b511a07c67a946de371b8cc0e238faa
tttien1812 <tttien204@gmail.com>
2025-03-26 15:54:43 +0700

    connect_data

--- src/server.js
+++ src/server.js
@@ -2,18 +2,23 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
-require('dotenv').config();
+import connectDB from "./config/connectDB";
+import { config } from "dotenv";
+
+require("dotenv").config();
 
 let app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 viewEngine(app);
 initWebRoutes(app);
 
+connectDB();
+
 let port = process.env.PORT || 6969;
 
-app.listen(port, ()=>{
-    console.log("backend nodejs is running on the port: " +port)
-})
+app.listen(port, () => {
+  console.log("backend nodejs is running on the port: " + port);
+});
