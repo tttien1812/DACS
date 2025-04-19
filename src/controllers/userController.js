3a0220297afb165277f7bf1b3c4bd94b6941d883
tttien1812 <tttien204@gmail.com>
2025-04-19 17:21:24 +0700

    API lay thong tin user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -19,7 +19,25 @@ let handletLogin = async (req, res) => {
     user: userData.user ? userData.user : {},
   });
 };
+let handleGetAllUsers = async (req, res) => {
+  let id = req.body.id;
 
+  if (!id) {
+    return res.status(200).json({
+      errCode: 1,
+      errMessage: "Missing required parameters",
+      users: [],
+    });
+  }
+
+  let users = await userService.getAllUsers(id);
+  return res.status(200).json({
+    errCode: 0,
+    errMessage: "ok",
+    users,
+  });
+};
 module.exports = {
   handletLogin: handletLogin,
+  handleGetAllUsers: handleGetAllUsers,
 };
--- src/route/web.js
+++ src/route/web.js
@@ -18,6 +18,7 @@ let initWebRoutes = (app) => {
   router.get("/delete-crud", homeController.deleteCRUD);
 
   router.post("/api/login", userController.handletLogin);
+  router.get("/api/get-all-users", userController.handleGetAllUsers);
 
   return app.use("/", router);
 };
