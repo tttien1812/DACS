640918b5198979cf10e6bd6457d1870d0e49fdfd
tttien1812 <tttien204@gmail.com>
2025-04-30 16:45:05 +0700

    add allcode

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -65,10 +65,24 @@ let handleEditUser = async (req, res) => {
   }
 };
 
+let getAllCode = async (req, res) => {
+  try {
+    let data = await userService.getAllCodeService(req.query.type);
+    return res.status(200).json(data);
+  } catch (e) {
+    consol.log("get all code error: ", e);
+    return res.status(200).json({
+      errCode: -1,
+      errMessage: "error from server",
+    });
+  }
+};
+
 module.exports = {
   handleLogin: handleLogin,
   handleGetAllUsers: handleGetAllUsers,
   handleCreateNewUser: handleCreateNewUser,
   handleEditUser: handleEditUser,
   handleDeleteUser: handleDeleteUser,
+  getAllCode: getAllCode,
 };
