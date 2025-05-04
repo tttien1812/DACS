d1c3c7a61dbaf9b57c8ca4c39b12144e571a2f12
tttien1812 <tttien204@gmail.com>
2025-05-05 02:29:24 +0700

    change another model and migration

--- src/services/userService.js
+++ src/services/userService.js
@@ -117,6 +117,7 @@ let createNewUser = (data) => {
           gender: data.gender,
           roleId: data.roleId,
           positionId: data.positionId,
+          image: data.avatar,
         });
         resolve({
           errCode: 0,
@@ -174,6 +175,9 @@ let updateUserData = (data) => {
         user.roleId = data.roleId;
         user.positionId = data.positionId;
         user.gender = data.gender;
+        if (data.avatar) {
+          user.image = data.avatar;
+        }
 
         await user.save();
         // await db.User.save({
