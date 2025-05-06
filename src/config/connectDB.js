0541d3795630acb56da43e2b7f3a0f25d0f23804
tttien1812 <tttien204@gmail.com>
2025-05-07 02:26:54 +0700

    add new model

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize("trunten", "root", null, {
+const sequelize = new Sequelize("FurCare", "root", null, {
   //"database", "username", "password",
   host: "localhost",
   dialect: "mysql",
