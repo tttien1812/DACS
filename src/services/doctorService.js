ae8f8229372bf1e1a981bed9beb3f909d1db5e72
tttien1812 <tttien204@gmail.com>
2025-05-09 16:06:43 +0700

    complete detaildoctor

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -62,19 +62,33 @@ let saveDetailInforDoctor = (inputData) => {
       if (
         !inputData.doctorId ||
         !inputData.contentHTML ||
-        !inputData.contentMarkdown
+        !inputData.contentMarkdown ||
+        !inputData.action
       ) {
         resolve({
           errCode: 1,
           errMessage: "Missing Parameter",
         });
       } else {
-        await db.Markdown.create({
-          contentHTML: inputData.contentHTML,
-          contentMarkdown: inputData.contentMarkdown,
-          description: inputData.description,
-          doctorId: inputData.doctorId,
-        });
+        if (inputData.action === "CREATE") {
+          await db.Markdown.create({
+            contentHTML: inputData.contentHTML,
+            contentMarkdown: inputData.contentMarkdown,
+            description: inputData.description,
+            doctorId: inputData.doctorId,
+          });
+        } else if (inputData.action === "EDIT") {
+          let doctorMarkdown = await db.Markdown.findOne({
+            where: { doctorId: inputData.doctorId },
+            raw: false,
+          });
+          if (doctorMarkdown) {
+            doctorMarkdown.contentHTML = inputData.contentHTML;
+            doctorMarkdown.contentMarkdown = inputData.contentMarkdown;
+            doctorMarkdown.description = inputData.description;
+            await doctorMarkdown.save();
+          }
+        }
 
         resolve({
           errCode: 0,
@@ -86,6 +100,7 @@ let saveDetailInforDoctor = (inputData) => {
     }
   });
 };
+
 let getDetailDoctorById = (inputId) => {
   return new Promise(async (resolve, reject) => {
     try {
@@ -134,6 +149,7 @@ let getDetailDoctorById = (inputId) => {
     }
   });
 };
+
 module.exports = {
   getTopDoctorHome: getTopDoctorHome,
   getAllDoctors: getAllDoctors,
