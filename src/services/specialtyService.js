e95620aa420091e71ee1f994c49a3550caac6f14
tttien1812 <tttien204@gmail.com>
2025-05-16 17:15:59 +0700

    api create specialty

---
+++ src/services/specialtyService.js
@@ -0,0 +1,37 @@
+import db from "../models/index";
+
+let createSpecialty = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (
+        !data.name ||
+        !data.imageBase64 ||
+        !data.descriptionHTML ||
+        !data.descriptionMarkdown
+      ) {
+        resolve({
+          errCode: 1,
+          errMessage: "Missing required Parameter!",
+        });
+      } else {
+        await db.Specialty.create({
+          name: data.name,
+          image: data.imageBase64,
+          descriptionHTML: data.descriptionHTML,
+          descriptionMarkdown: data.descriptionMarkdown,
+        });
+
+        resolve({
+          errCode: 0,
+          errMessage: "OK!",
+        });
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+module.exports = {
+  createSpecialty: createSpecialty,
+};
