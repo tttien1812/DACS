f8d7907d4578f52b07d1a5f0f55195c8ade937db
tttien1812 <tttien204@gmail.com>
2025-05-18 01:11:54 +0700

    fix clinic

--- src/route/web.js
+++ src/route/web.js
@@ -4,6 +4,7 @@ import userController from "../controllers/userController";
 import doctorController from "../controllers/doctorController";
 import patientController from "../controllers/patientController";
 import specialtyController from "../controllers/specialtyController";
+import clinicController from "../controllers/clinicController";
 
 let router = express.Router();
 
@@ -62,6 +63,21 @@ let initWebRoutes = (app) => {
     "/api/get-detail-specialty-by-id",
     specialtyController.getDetailSpecialtyById
   );
+
+  router.post("/api/create-new-clinic", clinicController.createClinic);
+  // router.get("/api/get-specialty", specialtyController.getAllSpecialty);
+  // router.get(
+  //   "/api/get-detail-specialty-by-id",
+  //   specialtyController.getDetailSpecialtyById
+  // );
+
+  //handbook
+  // router.post("/api/create-new-specialty", specialtyController.createSpecialty);
+  // router.get("/api/get-specialty", specialtyController.getAllSpecialty);
+  // router.get(
+  //   "/api/get-detail-specialty-by-id",
+  //   specialtyController.getDetailSpecialtyById
+  // );
 
   return app.use("/", router);
 };
